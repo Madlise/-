5d916ee1273c814935095634dfc3f5c7e4917044
Madlise <121611686+Madlise@users.noreply.github.com>
2023-01-27 16:09:32 +0300

    Add files via upload

---
+++ Task/Program.cs
@@ -0,0 +1,29 @@
+﻿string[] NewReduseArray(string[] array, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            array2[count] = array[i];
+            count++;
+        }
+    }
+    return array2;
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+string[] array = new string[5] { "33", "Lo", "666", "wizard", "42" };
+string[] array2 = new string[array.Length];
+
+string[] newArray = NewReduseArray(array, array2);
+PrintArray(array2);
